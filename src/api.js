8ebe4b1258567f49e1dbdd7dbc171ec305caa7e0
escX <1768810189@qq.coom>
2019-04-16 13:30:32 +0800

    modify api name & revert last commit

--- src/api.js
+++ src/api.js
@@ -3,5 +3,5 @@ import axios from "axios";
 axios.defaults.baseURL = "http://47.112.96.227:8080";
 
 export const getProjectInfo = projectName => {
-  return axios.get(`/github/project_info/${projectName}`);
+  return axios.get(`/github/project/${projectName}`);
 };
